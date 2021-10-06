9b5bf78aea39ab43b0fe397544f9667c1c92233c
camilaFetecua <maria.fetecua@mail.escuelaing.edu.co>
2021-10-05 21:36:56 -0500

    Agregando Tarea Terminado

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React, {useState} from 'react';
 import './App.css';
 import {TaskItem} from "./TaskItem";
 
+interface NewTaskParams {
+    name: any;
+}
+
 function App() {
     const [tasks, setTasks] = useState([
         {
@@ -17,22 +21,34 @@ function App() {
             name: "Keep on Keeping on"
         }
     ]);
-    const [textValue, setTextValue] = useState("");
+    const [textValue,setTextValue]=useState("");
+
     const handleTaskChange = (index:number) => () => {
         console.log("changed!" + index);
         const arr = [...tasks];
         arr[index].isCompleted = !arr[index].isCompleted;
         setTasks(arr);
     };
-    const handleSubmit =(event: { preventDefault: () => void; }) => {
+
+    const newTask = (name: string) =>{
+        const newTask = {
+            isCompleted: false,
+            name: name,
+        };
+
+        setTasks([...tasks,newTask]);
+    };
+
+    const handleSubmit =(event: { preventDefault: () => void; })=>{
         event.preventDefault();
         console.log(textValue);
+        newTask(textValue);
     };
-    const handleTextChange =( event: { target: { value: any; }; }) =>{
-        const value = event.target.value;
-        setTextValue(value);
-    }
 
+    const handleTextChange = (event: { target: { value: any; }; })=>{
+        const value =event.target.value;
+        setTextValue(value);
+    };
     return (
         <main>
             <form onSubmit={handleSubmit}>
